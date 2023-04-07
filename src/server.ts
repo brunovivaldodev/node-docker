8248c5d9661a930b62507a519364b6b87b765a1b
Bruno Vivaldo <brunolumeca@live.com>
2023-04-07 15:13:48 +0100

    chore: mongodb database connected

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from "express"
+import mongoose from "mongoose";
 
-const PORT =  process.env.PORT || 3000;
+import { config } from "./config"
+
+const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT, APP_PORT} = config
+
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(()=>console.log("Succesfully connected to DB"))
+    .catch((e)=>console.log(e))
+
 const app = express()
-
 
 app.get("/",(req,res)=> {
     res.send("<h2>Hi There!!</h2>")
 })
 
 
-app.listen(PORT,() => {
-    console.log(`Server Listening at port ${PORT}`)
+app.listen(APP_PORT,() => {
+    console.log(`Server Listening at port ${APP_PORT}`)
 })
